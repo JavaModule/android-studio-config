32ffb38a7222e7caac1978fa68cccb7845eb4402
Jackson Miras <jackmiras@gmail.com>
2017-04-29 00:43:21 -0400

    Removing unnecessary code from install-config.sh

--- install-config.sh
+++ install-config.sh
@@ -7,14 +7,12 @@ if [ -d "$DIRECTORY" ]; then
 	rm -rf $HOME/.AndroidStudio2.2/config/codestyles                            
 	rm -rf $HOME/.AndroidStudio2.2/config/colors                                
 	rm -rf $HOME/.AndroidStudio2.2/config/plugins                               
-	rm -rf $HOME/.AndroidStudio2.2/config/inspection                            
 	rm -rf $HOME/.AndroidStudio2.2/config/options                               
 
 	# Coping files new files into Android Studio config folder.
 	cp codestyles -r $HOME/.AndroidStudio2.2/config/
 	cp colors -r $HOME/.AndroidStudio2.2/config/
 	cp plugins -r $HOME/.AndroidStudio2.2/config/
-	cp inspection -r $HOME/.AndroidStudio2.2/config/
 	cp options -r $HOME/.AndroidStudio2.2/config/
 
 	echo "Installation finished with success!"
