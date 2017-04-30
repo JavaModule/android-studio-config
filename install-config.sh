3e8879031e46f4fd3ecd1d95408255c217b43628
Jackson Miras <jackmiras@gmail.com>
2017-04-30 19:33:42 -0400

    Updating the script that install this configs on Android Studio

--- install-config.sh
+++ install-config.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
-# Init
+# Script written by: JM.
+# This script install and configurations of Android Studio.
 
-DIRECTORY="$HOME/.AndroidStudio2.2"
+DIRECTORY="$HOME/.AndroidStudio2.3"
 if [ -d "$DIRECTORY" ]; then
-	# Removing some of the current configurations of Android Studio.                        
-	rm -rf $HOME/.AndroidStudio2.2/config/codestyles                            
-	rm -rf $HOME/.AndroidStudio2.2/config/colors                                
-	rm -rf $HOME/.AndroidStudio2.2/config/plugins                               
-	rm -rf $HOME/.AndroidStudio2.2/config/options                               
+  # Removing current configurations of Android Studio.
+  rm -rf $HOME/.AndroidStudio2.3/config/codestyles
+  rm -rf $HOME/.AndroidStudio2.3/config/colors
+  rm -rf $HOME/.AndroidStudio2.3/config/plugins
+  rm -rf $HOME/.AndroidStudio2.3/config/options/colors.scheme.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/debugger.renderers.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/debugger.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/editor.codeinsight.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/editor.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/ide.general.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/markdown_navigator.local.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/markdown_navigator.shared.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/multimarkdown.shared.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/path.macros.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/vim_settings.xml
 
-	# Coping files new files into Android Studio config folder.
-	cp codestyles -r $HOME/.AndroidStudio2.2/config/
-	cp colors -r $HOME/.AndroidStudio2.2/config/
-	cp plugins -r $HOME/.AndroidStudio2.2/config/
-	cp options -r $HOME/.AndroidStudio2.2/config/
+  # Creating a symbolic link of the new Android Studio configurations into .AndroidStudio folder.
+  cp codestyles/ -R $HOME/.AndroidStudio2.3/config/
+  cp colors/ -R $HOME/.AndroidStudio2.3/config/
+  cp plugins/ -R $HOME/.AndroidStudio2.3/config/
+  cp options/colors.scheme.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/debugger.renderers.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/debugger.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/editor.codeinsight.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/editor.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/ide.general.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/markdown_navigator.local.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/markdown_navigator.shared.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/multimarkdown.shared.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/path.macros.xml -R $HOME/.AndroidStudio2.3/config/options/
+  cp options/vim_settings.xml -R $HOME/.AndroidStudio2.3/config/options/
 
 	echo "Installation finished with success!"
 else
