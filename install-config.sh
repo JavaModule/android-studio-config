6231a84551a5b602bfd0cd369d1023393b517c07
Jack Miras <jackmiras@gmail.com>
2017-08-27 23:16:18 -0400

    Updating the code into the install-configs.sh script

--- install-config.sh
+++ install-config.sh
@@ -2,41 +2,50 @@
 # Script written by: JM.
 # This script install and configurations of Android Studio.
 
-DIRECTORY="$HOME/.AndroidStudio2.3"
-if [ -d "$DIRECTORY" ]; then
+if [ -d "$HOME/.AndroidStudio2.3" ]; then
   # Removing current configurations of Android Studio.
-  rm -rf $HOME/.AndroidStudio2.3/config/codestyles
+	rm -rf $HOME/.AndroidStudio2.3/config/codestyles
   rm -rf $HOME/.AndroidStudio2.3/config/colors
-  rm -rf $HOME/.AndroidStudio2.3/config/plugins
+  rm -rf $HOME/.AndroidStudio2.3/config/fileTemplates
   rm -rf $HOME/.AndroidStudio2.3/config/options/colors.scheme.xml
   rm -rf $HOME/.AndroidStudio2.3/config/options/debugger.renderers.xml
   rm -rf $HOME/.AndroidStudio2.3/config/options/debugger.xml
   rm -rf $HOME/.AndroidStudio2.3/config/options/editor.codeinsight.xml
   rm -rf $HOME/.AndroidStudio2.3/config/options/editor.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/find.xml
   rm -rf $HOME/.AndroidStudio2.3/config/options/ide.general.xml
-  rm -rf $HOME/.AndroidStudio2.3/config/options/markdown_navigator.local.xml
-  rm -rf $HOME/.AndroidStudio2.3/config/options/markdown_navigator.shared.xml
-  rm -rf $HOME/.AndroidStudio2.3/config/options/multimarkdown.shared.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/jdk.table.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/laf.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/other.xml
   rm -rf $HOME/.AndroidStudio2.3/config/options/path.macros.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/project.default.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/options/ui.lnf.xml
+  rm -rf $HOME/.AndroidStudio2.3/config/templates
+  rm -rf $HOME/.AndroidStudio2.3/config/plugins
 
-  # Creating a symbolic link of the new Android Studio configurations into .AndroidStudio folder.
-  cp codestyles/ -R $HOME/.AndroidStudio2.3/config/
-  cp colors/ -R $HOME/.AndroidStudio2.3/config/
-  cp plugins/ -R $HOME/.AndroidStudio2.3/config/
-  cp options/colors.scheme.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/debugger.renderers.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/debugger.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/editor.codeinsight.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/editor.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/ide.general.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/markdown_navigator.local.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/markdown_navigator.shared.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/multimarkdown.shared.xml -R $HOME/.AndroidStudio2.3/config/options/
-  cp options/path.macros.xml -R $HOME/.AndroidStudio2.3/config/options/
-
-	echo "Installation finished with success!"
+  # Copping files and folders from custom configurations into .AndroidStudio2.3 folder.
+	cp -R $HOME/Projects/android-studio-config/codestyles/ $HOME/.AndroidStudio2.3/config/
+  cp -R $HOME/Projects/android-studio-config/colors/ $HOME/.AndroidStudio2.3/config/
+  cp -R $HOME/Projects/android-studio-config/fileTemplates/ $HOME/.AndroidStudio2.3/config/
+  cp $HOME/Projects/android-studio-config/options/colors.scheme.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/debugger.renderers.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/debugger.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/editor.codeinsight.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/editor.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/find.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/ide.general.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/jdk.local.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/laf.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/other.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/path.macros.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/project.default.xml $HOME/.AndroidStudio2.3/config/options/
+  cp $HOME/Projects/android-studio-config/options/ui.lnf.xml $HOME/.AndroidStudio2.3/config/options/
+  cp -R $HOME/Projects/android-studio-config/templates/ $HOME/.AndroidStudio2.3/config/
+  cp -R $HOME/Projects/android-studio-config/plugins/ $HOME/.AndroidStudio2.3/config/
+  
+  echo "Installation finished with success!"
 else
-	echo "The directory .AndroidStudio2.2 doesn't exists... You need to start"\
+	echo "The directory .AndroidStudio2.3 doesn't exists... You need to start"\
 			 "Android Studio at least once before try to install this configs"
 fi
 
